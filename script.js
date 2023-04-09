dd7229dd74ea5b55b5dc4f17e2db5e223b40889d
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-04-09 15:04:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const firstName = form.elements['First Name'].value;
+  const lastName = form.elements['Last Name'].value;
+  const phoneNumber = form.elements['Phone Number'].value;
+  const email = form.elements['Email ID'].value;
+
+  const message = `First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${email}`;
+
+  alert(message);
+});
